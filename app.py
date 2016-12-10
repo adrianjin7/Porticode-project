df7c86e513e3634f558e30edccc77f8a4baadad3
kaijuburgers <cwldrdge@eduroam-int-dhcp-97-217-234.ucl.ac.uk>
2016-12-10 17:48:17 +0000

    addding results and foot/head html

--- app.py
+++ app.py
@@ -2,3 +2,15 @@ from flask import render_template, Flask
 from datafoo import spotify
 
 app = Flask(__name__)
+
+@app.royte('/')
+def index():
+    html = render('index.html')
+    return html
+
+@app.route('/search/<city>')
+def search(country):
+    html = render('search.html',
+                    artist_name=name,
+                    results=items,
+                    api_url=api_url)
