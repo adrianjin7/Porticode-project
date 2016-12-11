fa2116b5030a8d817af1bcc778f75c2d3bf205ab
kaijuburgers <cwldrdge@eduroam-int-dhcp-97-217-234.ucl.ac.uk>
2016-12-11 09:16:42 +0000

    added result.html

--- map.js
+++ map.js
@@ -1,12 +1,12 @@
 var geocoder;
 var map;
-var dest = document.getElementById("inputCity").value;
-var country = document.getElementById("inputCountry").value;
-var address = dest.concat(" " + country);
+//var dest = document.getElementById("inputCity").value;//
+//var country = document.getElementById("inputCountry").value;
+//var address = dest.concat(" " + country);
 
 function initialize() {
   geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var latlng = new google.maps.LatLng(40.730610, -73.935242);
   var myOptions = {
     zoom: 8,
     center: latlng,
@@ -15,6 +15,7 @@ function initialize() {
       style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
     },
     navigationControl: true,
+    backgroundColor: 'red',
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
@@ -50,4 +51,3 @@ function initialize() {
   }
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-    
